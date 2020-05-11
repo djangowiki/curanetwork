11988f64c85ae9777bfc22aefd8bc1a8e20a3dc0
djangowiki <flixya4gabby@gmail.com>
2020-05-11 19:16:50 +0100

    deployment and fixes

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PropertyViewSet, BookingViewSet
+from .views import PropertyViewSet, BookingViewSet, location
 
 router = routers.DefaultRouter()
 router.register('bookings', BookingViewSet)
 router.register('properties', PropertyViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('properties?at=lat,long', location)
+
 ]
--- api/views.py
+++ api/views.py
@@ -10,6 +10,7 @@ import requests
 import json
 
 
+# Property Viewset
 class PropertyViewSet(viewsets.ModelViewSet):
     serializer_class = PropertyModelSerializer
     queryset = Property.objects.all()
@@ -23,17 +24,17 @@ class PropertyViewSet(viewsets.ModelViewSet):
         response = {'message': 'Property Bookings', 'result': serializer.data}
         return Response(response, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['GET', 'POST'])
-    def getlocation(self, request):
-        api_service_call = requests.get('https://places.ls.hereapi.com/places/v1/autosuggest?at=40.74917,'
-                                        '-73.98529&q=chrysler&apiKey=lCd5ubBwiQhPFnb06L9CxybSfoOkRQQdLQWO_6KVfnA')
 
-        api_parser = json.loads(api_service_call.content)
-        response = {'api Data': api_parser}
-        return Response(response, status.HTTP_200_OK)
-
-
+# Bookings Viewset
 class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingModelSerializer
     queryset = Booking.objects.all()
-    permission_classes = (AllowAny,)
+    permission_classes = (AllowAny,)  # Application API is open at the moment.
+
+
+def location(self, request):
+    api_service_call = requests.get('https://places.ls.hereapi.com/places/v1/autosuggest?at=40.74917,'
+                                    '-73.98529&q=chrysler&apiKey=lCd5ubBwiQhPFnb06L9CxybSfoOkRQQdLQWO_6KVfnA')
+    api_parser = json.loads(api_service_call.content)
+    response = {'api Data': api_parser}
+    return Response(response, status.HTTP_200_OK)
